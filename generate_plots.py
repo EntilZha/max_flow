ba94fb03729150061748a3b2a4d43c81fe5dc153
Pedro Rodriguez <ski.rodriguez@gmail.com>
2016-04-27 00:23:26 -0600

    Finished report and modified plots

--- generate_plots.py
+++ generate_plots.py
@@ -1,5 +1,6 @@
 from collections import namedtuple
 import matplotlib.pyplot as plt
+from scipy import stats
 from fn import _
 from functional import seq
 import numpy as np
@@ -18,6 +19,8 @@ def plot(filename):
     dfs = data.filter(_.algorithm == 'dfs')
     x = np.array(bfs.map(lambda x: x.vertexes * x.edges * x.edges).list())
     y = np.array(bfs.map(_.runtime).list())
+    slope, intercept, r_value, p_value, std_err = stats.linregress(x, y)
+    print(slope, intercept, r_value, p_value, std_err)
     plt.title('Numerical Performance of Edmonds-Karp')
     plt.xlabel('Input Size in VE^2')
     plt.ylabel('Running Time in Seconds')
@@ -29,10 +32,21 @@ def plot(filename):
     plt.xlabel('Input Size in Ef')
     plt.ylabel('Running Time in Seconds')
     max_flow = ff_data.max_by(lambda kv: kv[0])[0]
+    all_x = list()
+    all_y = list()
     for k, v in ff_data:
-        x = list(map(_[0], v))
-        y = list(map(_[1], v))
-        plt.scatter(x, y, color=str(k / max_flow))
+        x = list(map(_[1], v))
+        all_x.extend(x)
+        y = list(map(_[2], v))
+        all_y.extend(y)
+        ratio = 1 - k / max_flow
+        if ratio > .8:
+            ratio = .8
+        plt.scatter(x, y, color=str(ratio))
+    x = np.array(all_x)
+    y = np.array(all_y)
+    slope, intercept, r_value, p_value, std_err = stats.linregress(x, y)
+    print(slope, intercept, r_value, p_value, std_err)
     plt.show()
 
 
